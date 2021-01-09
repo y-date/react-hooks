7e16ea7f69b17dbfcb269f473b021dc8d230db90
Yohei Date <itachi.0101y@gmail.com>
2021-01-09 22:38:43 +0900

    implement all events deletion feature

--- src/components/Event.js
+++ src/components/Event.js
@@ -3,7 +3,8 @@ import React from 'react';
 const Event = ({event, dispatch} ) => {
   const id = event.id;
   const handleClickDeleteButton = () => {
-    dispatch({type: 'DELETE_EVENT', id});
+    const result = window.confirm(`イベント(ID:${id})を本当に削除しますか？`);
+    if (result) dispatch({type: 'DELETE_EVENT', id});
   }
 
   return (
