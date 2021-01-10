690bfcdd7fd442a88747300bb77aa0033932b52c
Yohei Date <itachi.0101y@gmail.com>
2021-01-10 15:39:47 +0900

    delete all operation logs

--- src/components/EventForm.js
+++ src/components/EventForm.js
@@ -44,6 +44,16 @@ const EventForm = () => {
     }
   }
 
+  const deleteAllOperationLogs = e => {
+    e.preventDefault();
+    const result = window.confirm('全ての操作ログを本当に削除しますか？');
+    if (result) {
+      dispatch({
+        type: DELETE_ALL_OPERATION_LOGS,
+      });
+    }
+  }
+
   const unCreatable = title === '' || body === '';
 
   return (
@@ -62,7 +72,7 @@ const EventForm = () => {
 
         <button className="btn btn-primary" onClick={addEvent} disabled={unCreatable}>イベントを作成する</button>
         <button className="btn btn-danger" onClick={deleteAllEvents} disabled={state.events.length === 0}>全てのイベントを削除する</button>
-        {/* <button className="btn btn-danger">全てのイベントを削除する</button> */}
+        <button className="btn btn-danger" onClick={deleteAllOperationLogs} disabled={state.operationLogs.length === 0}>全ての操作ログを削除する</button>
       </form>
     </>
   )
