36054d65113587fcd8ddd944ec833eb861457a0d
Yohei Date <itachi.0101y@gmail.com>
2021-01-10 16:06:35 +0900

    display all operation logs

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import React, { useReducer } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import EventForm from './EventForm';
 import Events from './Events';
+import OperationLogs from './OperationLogs';
 import AppContext from '../contexts/AppContext';
 import reducer from '../reducers/';
 
@@ -17,6 +18,7 @@ const App = () => {
       <div className="container-fluid">
         <EventForm />
         <Events />
+        <OperationLogs />
       </div>
     </AppContext.Provider>
   );
