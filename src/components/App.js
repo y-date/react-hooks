4f7ddb5a80597a4c9c8f00e30c2ccdc2ad7bdb88
Yohei Date <itachi.0101y@gmail.com>
2021-01-10 13:35:43 +0900

    refactor events with reducers

--- src/components/App.js
+++ src/components/App.js
@@ -4,11 +4,13 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import EventForm from './EventForm';
 import Events from './Events';
 import AppContext from '../contexts/AppContext';
-import reducer from '../reducers';
+import reducer from '../reducers/';
 
 const App = () => {
-  const [state, dispatch] = useReducer(reducer, []);
-
+  const initialState = {
+    events: []
+  }
+  const [state, dispatch] = useReducer(reducer, initialState);
   return (
     <AppContext.Provider value={{state, dispatch}}>
       <div className="container-fluid">
